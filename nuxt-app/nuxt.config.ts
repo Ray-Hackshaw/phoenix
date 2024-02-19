7e1209984723d8f02f820066f06be1ef78b500fc
notray.hackshaw@gmail.com <36725950+Ray-Hackshaw@users.noreply.github.com>
2024-02-20 00:19:04 +1300

    test: build

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  routeRules: {
+    // prerender index route by default
+    "/": { prerender: true },
+  },
   typescript: {
     typeCheck: true,
   },
