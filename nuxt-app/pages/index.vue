815445ffdc6d2443a5043787ca36bf89b6d5a619
Ray-Hackshaw <ray.hackshaw@gmail.com>
2024-10-13 23:56:20 +1300

    fix: minor

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -25,7 +25,7 @@ useHead({
         <div
           class="max-w-5xl w-full mx-auto min-h-[30vh] p-4 rounded-md relative"
         >
-          <div class="flex flex-col md:flex-row md:gap-4 w-full">
+          <div class="flex flex-col lg:flex-row lg:gap-4 w-full">
             <div class="relative font-bold uppercase text-4xl md:text-6xl">
               <h1 class="tracking-tight text-white">Ray Hackshaw</h1>
               <p
@@ -34,56 +34,68 @@ useHead({
                 Ray Hackshaw
               </p>
             </div>
-            <div class="indent-2 leading-tight uppercase md:pb-0 pb-4">
-              <h2 class="text-base md:text-xl">Full-stack Developer</h2>
-              <p class="text-xs md:text-sm">Auckland, NZ</p>
+            <div class="indent-2 leading-tight md:pb-0 pb-4">
+              <h2 class="text-base md:text-xl uppercase">
+                Full-stack Developer
+              </h2>
+              <p class="text-xs md:text-sm uppercase">Auckland, NZ</p>
               <div class="text-white w-full py-1">
                 <VerticalTextCarousel />
               </div>
+              <p>ray@rayhackshaw.com</p>
             </div>
           </div>
         </div>
       </div>
       <div
-        className="flex gap-4 lg:flex-col items-center lg:items-start justify-center p-4"
+        class="flex gap-4 lg:flex-col items-center lg:items-start justify-center p-4 w-full lg:w-fit"
       >
-        <NuxtLink to="https://github.com/Ray-Hackshaw" target="_blank">
-          <div class="flex gap-2 items-center">
-            <NuxtImg
-              src="/github.svg"
-              width="20"
-              height="20"
-              class="text-white fill-white stroke-white"
-            />
-            <p class="text-white hidden lg:flex">Github</p>
-          </div>
-        </NuxtLink>
-        <NuxtLink to="https://www.linkedin.com/in/rayhackshaw/" target="_blank">
-          <div class="flex gap-2 items-center">
-            <NuxtImg
-              src="/linkedin.svg"
-              width="20"
-              height="20"
-              class="text-white fill-white stroke-white"
-              alt="linkedin logo"
-            />
-            <p class="text-white hidden lg:flex">LinkedIn</p>
-          </div>
-        </NuxtLink>
-        <NuxtLink to="https://reading.supply/@rhackshaw" target="_blank">
-          <div class="flex gap-2 items-center">
-            <NuxtImg
-              src="/readingsupply.svg"
-              width="20"
-              height="20"
-              class="text-white fill-white stroke-white"
-              alt="ReadingSupply logo"
-            />
-            <p class="text-white hidden lg:flex">ReadingSupply</p>
-          </div>
-        </NuxtLink>
+        <div>
+          <NuxtLink to="https://github.com/Ray-Hackshaw" target="_blank">
+            <div class="flex gap-2 items-center">
+              <NuxtImg
+                src="/github.svg"
+                width="20"
+                height="20"
+                class="text-white fill-white stroke-white"
+              />
+              <p class="text-white hidden lg:flex">Github</p>
+            </div>
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="https://www.linkedin.com/in/rayhackshaw/"
+            target="_blank"
+          >
+            <div class="flex gap-2 items-center">
+              <NuxtImg
+                src="/linkedin.svg"
+                width="20"
+                height="20"
+                class="text-white fill-white stroke-white"
+                alt="linkedin logo"
+              />
+              <p class="text-white hidden lg:flex">LinkedIn</p>
+            </div>
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink to="https://reading.supply/@rhackshaw" target="_blank">
+            <div class="flex gap-2 items-center">
+              <NuxtImg
+                src="/readingsupply.svg"
+                width="20"
+                height="20"
+                class="text-white fill-white stroke-white"
+                alt="ReadingSupply logo"
+              />
+              <p class="text-white hidden lg:flex">ReadingSupply</p>
+            </div>
+          </NuxtLink>
+        </div>
       </div>
-      <div class="absolute bottom-0 py-4 flex gap-1">
+      <div class="absolute bottom-4 py-4 flex gap-1">
         <p class="text-white">Analytics powered by</p>
         <NuxtLink to="https://denki.co.nz/" class="underline text-white"
           >Denki</NuxtLink
